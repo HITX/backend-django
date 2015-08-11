9e4691bd505dad6a3959caf80a118c5be0afe695
Ben Parham <ben@playlist.com>
2015-08-11 00:37:45 -0700

    Minor changes to gitignore and settings init

--- apiserver/settings/__init__.py
+++ apiserver/settings/__init__.py
@@ -1,9 +1,12 @@
 # application_python cookbook expects manage.py in a top level
 # instead of app level dir, so the relative import can fail
-try:
-    from .backend-django.apiserver.settings.base import *
-except ImportError:
-    from apiserver.settings.base import *
+
+# try:
+#     from .backend-django.apiserver.settings.base import *
+# except ImportError:
+#     from apiserver.settings.base import *
+
+from apiserver.settings.base import *
 
 try:
     from local_settings import *
