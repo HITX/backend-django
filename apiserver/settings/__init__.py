b563d2ce3940f5491d5f9db1de1494c9381b568d
Ben Parham <ben@playlist.com>
2015-08-11 00:15:57 -0700

    Add code from prexisting public repo

---
+++ apiserver/settings/__init__.py
@@ -0,0 +1,11 @@
+# application_python cookbook expects manage.py in a top level
+# instead of app level dir, so the relative import can fail
+try:
+    from .backend-django.apiserver.settings.base import *
+except ImportError:
+    from apiserver.settings.base import *
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
