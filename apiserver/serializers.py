a00799f8e3e7db90b1636af697d3980697bcd3cc
Ben Parham <ben@playlist.com>
2015-08-25 23:41:47 -0700

    Move error messages into mixin. Clean up user serializer

--- apiserver/serializers.py
+++ apiserver/serializers.py
@@ -2,14 +2,16 @@ from django.contrib.auth.models import Group
 
 from django.core.validators import RegexValidator
 
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
 from rest_framework.validators import UniqueValidator
 
 from apiserver.models import User
 from apiserver.validators import RequiredValidator
 from profiles.serializers import ProfileSerializer
 
-class UserSerializer(serializers.ModelSerializer):
+from mixins.serializers import ErrorMessages
+
+class UserSerializer(ErrorMessages, ModelSerializer):
     profile = ProfileSerializer(required=False)
 
     class Meta:
@@ -26,66 +28,25 @@ class UserSerializer(serializers.ModelSerializer):
         }
         error_messages = {
             'username': {
-                'names': {
-                    'required': 'Test username required message'
-                },
+                'names': { 'required': 'Username is required' },
                 'validators': {
                     UniqueValidator: 'A user with that username already exists',
                     RegexValidator: 'Invalid username'
                 }
+            },
+            'email': {
+                'names': { 'required': 'Email is required' },
+                'validators': { UniqueValidator: 'A user with that email already exists' }
+            },
+            'password': {
+                'names': { 'required': 'Password is required' }
             }
         }
-        # validators = [
-        #     RequiredValidator(fields=('username', 'email', 'password'))
-        # ]
 
     def create(self, validated_data):
         return User.objects.create(validated_data)
 
-    def __init__(self, *args, **kwargs):
-        super(UserSerializer, self).__init__(*args, **kwargs)
 
-        # print self.fields['username'].error_messages
-        # print self.fields['username'].validators
-        # self.fields['email'].error_messages['required'] = 'test required message'
-        # self.fields['username'].validators[1].message = 'test unique message'
-
-        # name_messages = self.Meta.error_messages.names
-        # validator_messages = self.Meta.error_messages.validators
-        #
-        # print name_messages
-        # print validator_messages
-
-        messages = self.Meta.error_messages
-        # print messages
-
-        for field_name in messages:
-
-            # print field_name + ':'
-
-            # print messages[field_name]
-            # print messages[field_name]['names']
-
-            # Handle names
-            for name, message in messages[field_name]['names'].items():
-                # print name + ' - ' + message
-                self.fields[field_name].error_messages[name] = message
-
-            # Handle validators
-            validator_keys = messages[field_name]['validators']
-            for validator in self.fields[field_name].validators:
-                if type(validator) in validator_keys:
-                    validator.message = validator_keys[type(validator)]
-
-        # for field_name, validator_keys in messages.items():
-        #     print field_name
-        #     print self.fields[field_name].error_messages
-        #
-        #     for validator in self.fields[field_name].validators:
-        #         if type(validator) in validator_keys:
-        #             validator.message = validator_keys[type(validator)]
-
-
-class GroupSerializer(serializers.ModelSerializer):
+class GroupSerializer(ModelSerializer):
     class Meta:
         model = Group
