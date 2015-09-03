468c2f4f9bf2ddd0959a6d679768decd844021eb
Ben Parham <ben@playlist.com>
2015-09-03 04:13:55 -0700

    Add defaults to profiles for testing

--- profiles/models.py
+++ profiles/models.py
@@ -4,15 +4,15 @@ from django.conf import settings
 
 class InternProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name = 'intern_profile')
-    first_name = models.CharField(max_length=50, blank=True)
-    last_name = models.CharField(max_length=50, blank=True)
+    first_name = models.CharField(max_length=50, default='Thomas')
+    last_name = models.CharField(max_length=50, default='Anderson')
 
     class Meta:
         db_table = 'intern_profile'
 
 class OrgProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name = 'org_profile')
-    org_name = models.CharField(max_length=50, blank=True)
+    org_name = models.CharField(max_length=50, default='Initech Inc.')
 
     class Meta:
         db_table = 'org_profile';
